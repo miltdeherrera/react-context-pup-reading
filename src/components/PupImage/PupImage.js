887d75a540305b0e069cd9d74a7744bfff7b9990
Milt Deherrera <miltdeherrera@gmail.com>
2022-11-15 14:22:33 -0800

    add provider wrapper, connect pup images

--- src/components/PupImage/PupImage.js
+++ src/components/PupImage/PupImage.js
@@ -1,10 +1,16 @@
+import { useContext } from 'react';
+import { PupContext } from '../../context/PupContext';
+
 import speedy from '../../pups/speedy-pup.jpg';
 import banana from '../../pups/banana-pup.jpg';
 import sleepy from '../../pups/sleepy-pup.jpg';
 
 const PupImage = () => {
+  const { puppyType } = useContext(PupContext);
+  console.log(puppyType)
+
   return (
-    <img src={speedy} alt="pup" />
+    <img src={puppyType} alt="pup" />
   );
 };
 
--- src/context/PupContext.js
+++ src/context/PupContext.js
@@ -20,11 +20,13 @@
 
 import { createContext, useState } from 'react';
 import speedy from '../pups/speedy-pup.jpg';
+import banana from '../pups/banana-pup.jpg';
+import sleepy from '../pups/sleepy-pup.jpg';
 
 export const PupContext = createContext();
 
 export const PupProvider = (props) => {
-  const [puppyType, setPuppyType] = useState(speedy);
+  const [puppyType, setPuppyType] = useState(banana);
 
   return (
     <PupContext.Provider
