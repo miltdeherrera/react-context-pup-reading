388b87e4a6baf9263a9273148d4d20bec3af3257
Milt Deherrera <miltdeherrera@gmail.com>
2022-11-15 13:18:15 -0800

    add context

--- src/context/PupContext.js
+++ src/context/PupContext.js
@@ -1,18 +1,23 @@
-import { createContext, useState, useContext } from 'react'
-import speedy from '../pups/speedy-pup.jpg';
+// import { createContext, useState, useContext } from 'react'
+// import speedy from '../pups/speedy-pup.jpg';
+
+// export const PupContext = createContext();
+
+// export function PupProvider(props) {
+//   const [puppyType, setPuppyType] = useState(speedy);
+
+//   return (
+//     <PupContext.Provider value={{ puppyType, setPuppyType }}>
+//       {props.children}
+//     </PupContext.Provider>
+//   )
+// }
+
+// export function usePuppyType() {
+//   return useContext(PupContext);
+// }
+
+
+import { createContext } from 'react';
 
 export const PupContext = createContext();
-
-export function PupProvider(props) {
-  const [puppyType, setPuppyType] = useState(speedy);
-
-  return (
-    <PupContext.Provider value={{ puppyType, setPuppyType }}>
-      {props.children}
-    </PupContext.Provider>
-  )
-}
-
-export function usePuppyType() {
-  return useContext(PupContext);
-}
